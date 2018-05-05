14db4a5cc9ed7ce95f4d90ed24184bbc5c4f0876
Bojan17 <bojan.kovacevic.pv@gmail.com>
2018-05-04 18:24:12 -0800

    second commit

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,7 +2,6 @@ import media
 import fresh_tomatoes
 
 toy_story = media.Movie("Toy Sorie",
-                        "A story of a boy and his toys that came to life",
                         "http://upload.wikimedia.or/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=vwyZH85NQC4")
 
@@ -10,21 +9,19 @@ toy_story = media.Movie("Toy Sorie",
 
 
 avatar = media.Movie("Avatar",
-                     "A marine on alien planet",
                      "http://upload/wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser_Poster.jpg",
                      "http://www.youtube.com/watch?v=-9ceBgWV8io")
 
 
-godfather = media.Movie("GodFather", "Storyline",
+godfather = media.Movie("GodFather",
                              "https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg",
                              "https://www.youtube.com/watch?v=sY1S34973zA")
 
-ratatouille = media.Movie("Ratatoullie", "Storyline",
+ratatouille = media.Movie("Ratatoullie",
                              "http://upload/wikimedia.org/wikipedia/id/b/b0/RatatoulliePoster.jpg",
                              "https://www.youtube.com/watch?v=c#sBBRxDAqk")
 
 tubelight = media.Movie("Tubelight",
-                        "A war time drama film",
                         "https://en.wikipedia.org/wiki/File:Tubelight_Poster.jpg",
                         "https://www.youtube.com/watch?v=PGQRNKHJwH4")
 
