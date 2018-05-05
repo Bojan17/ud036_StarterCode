a0f0f8fb816e49fc0419cfcab139793097212e8b
Bojan17 <bojan.kovacevic.pv@gmail.com>
2018-05-05 11:34:32 -0800

    error fixed

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,29 +1,19 @@
 import media
 import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story",
-                        "http://upload.wikimedia.or/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
+toy_story = media.Movie("Toy Story","http://upload.wikimedia.or/wikipedia/en/1/13/Toy_Story.jpg","https://www.youtube.com/watch?v=vwyZH85NQC4")
 
 #print(toy_story.storyline)
 
 
-avatar = media.Movie("Avatar",
-                     "http://upload/wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser_Poster.jpg",
-                     "http://www.youtube.com/watch?v=-9ceBgWV8io")
+avatar = media.Movie("Avatar","http://upload/wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser_Poster.jpg","http://www.youtube.com/watch?v=-9ceBgWV8io")
 
 
-godfather = media.Movie("GodFather",
-                             "https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg",
-                             "https://www.youtube.com/watch?v=sY1S34973zA")
+godfather = media.Movie("GodFather","https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg","https://www.youtube.com/watch?v=sY1S34973zA")
 
-ratatouille = media.Movie("Ratatoullie",
-                             "http://upload/wikimedia.org/wikipedia/id/b/b0/RatatoulliePoster.jpg",
-                             "https://www.youtube.com/watch?v=c#sBBRxDAqk")
+ratatouille = media.Movie("Ratatoullie","http://upload/wikimedia.org/wikipedia/id/b/b0/RatatoulliePoster.jpg","https://www.youtube.com/watch?v=c#sBBRxDAqk")
 
-tubelight = media.Movie("Tubelight",
-                        "https://en.wikipedia.org/wiki/File:Tubelight_Poster.jpg",
-                        "https://www.youtube.com/watch?v=PGQRNKHJwH4")
+tubelight = media.Movie("Tubelight","https://en.wikipedia.org/wiki/File:Tubelight_Poster.jpg","https://www.youtube.com/watch?v=PGQRNKHJwH4")
 
 
 movies = [toy_story, avatar, godfather, ratatoullie,tubelight]
